d0ff69c3f117420bce930f12859d908ff5aacfc1
Arushi Gupta <arushigupta5104@gmail,com>
2025-07-13 02:30:53 +0530

    added modules

---
+++ src/constant/__init__.py
@@ -0,0 +1,13 @@
+import os
+
+AWS_S3_BUCKET_NAME = "wafer-fault"
+MONGO_DATABASE_NAME = "iitk"
+MONGO_COLLECTION_NAME = "waferfault"
+
+TARGET_COLUMN = "quality"
+MONGO_DB_URL = "mongodb+srv://Manvi:12345@cluster0.u46c4.mongodb.net/?retryWrites=true&w=majority"
+
+MODEL_FILE_NAME = "model"
+MODEL_FILE_EXECUTION = ".pkl"
+
+artifact_folder = "artifacts"
